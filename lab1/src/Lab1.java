fb9dbf858f99b9695d93f5db0981cfef84cac341
Kristoffer Ek <kristofferboek@gmail.com>
2018-01-16 15:17:45 +0100

    Initial project files

---
+++ lab1/src/Lab1.java
@@ -0,0 +1,17 @@
+import TSim.*;
+
+public class Lab1 {
+
+  public Lab1(Integer speed1, Integer speed2) {
+    TSimInterface tsi = TSimInterface.getInstance();
+
+    try {
+      System.out.println("Hello");
+      tsi.setSpeed(1,speed1);
+    }
+    catch (CommandException e) {
+      e.printStackTrace();    // or only e.getMessage() for the error
+      System.exit(1);
+    }
+  }
+}
