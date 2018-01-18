0fd3f0627025c860d58dac9d28b2bac121962133
Kristoffer Ek <kristofferboek@gmail.com>
2018-01-18 14:32:02 +0100

    simple thread

--- lab1/src/Lab1.java
+++ lab1/src/Lab1.java
@@ -3,13 +3,32 @@ import TSim.*;
 public class Lab1 {
 
   public Lab1(Integer speed1, Integer speed2) {
-    TSimInterface tsi = TSimInterface.getInstance();
+
+    TrainThread t1 = new TrainThread(1, speed1);
+    TrainThread t2 = new TrainThread(2, speed2);
+    t1.start();
+    t2.start();
+  }
+}
+
+class TrainThread extends Thread {
+  TSimInterface tsi = TSimInterface.getInstance();
+  int id;
+  int speed;
+
+  public TrainThread(int id, int speed){
+    this.id = id;
+    this.speed = speed;
+  }
+
+  @Override
+  public void run() {
+    System.out.println("Hello, I'm train " + id);
 
     try {
-      System.out.println("Hello");
-      tsi.setSpeed(1,speed1);
+      tsi.setSpeed(id,speed);
     }
-    catch (CommandException e) {
+      catch (CommandException e) {
       e.printStackTrace();    // or only e.getMessage() for the error
       System.exit(1);
     }
